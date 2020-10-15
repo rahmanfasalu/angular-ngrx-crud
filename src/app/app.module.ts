1846f9c7caf75798bfd2b62c960e65025bea4fc9
Fasalu Rahman <fasalukm@gmail.com>
2020-10-15 08:31:20 +0200

    added normalizr and ngrx package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { MenuComponent } from './shared/menu.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutUsComponent } from './about-us/about-us/about-us.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,7 @@ import { AboutUsComponent } from './about-us/about-us/about-us.component';
     MenuComponent,
     AboutUsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, StoreModule.forRoot({}, {}), EffectsModule.forRoot([]), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
